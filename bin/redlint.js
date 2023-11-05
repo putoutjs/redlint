b6864a43a329db0db554542a35dbf43a1c06f24f
coderaiser <coderaiser@cloudcmd.io>
2023-11-05 11:59:20 +0200

    chore: lint

--- bin/redlint.js
+++ bin/redlint.js
@@ -1,7 +1,9 @@
+import {lintJSON} from 'putout/lint/json';
+
 import process from 'node:process';
 import {writeFile} from 'node:fs/promises';
+
 import {buildTree} from '../lib/redlint.js';
-import {lintJSON} from 'putout/lint/json';
 
 const {stringify} = JSON;
 const result = await buildTree(process.cwd());
