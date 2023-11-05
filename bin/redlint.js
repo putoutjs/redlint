f3ac84921c3b7333ef4dace098d03d2123dca4d0
coderaiser <coderaiser@cloudcmd.io>
2023-11-05 11:56:45 +0200

    feature: redlint: ignore

--- bin/redlint.js
+++ bin/redlint.js
@@ -1,8 +1,10 @@
 import process from 'node:process';
 import {writeFile} from 'node:fs/promises';
 import {buildTree} from '../lib/redlint.js';
+import {lintJSON} from 'putout/lint/json';
 
+const {stringify} = JSON;
 const result = await buildTree(process.cwd());
-const filesystem = JSON.stringify(result, null, 4) + '\n';
+const filesystem = lintJSON(stringify(result));
 
 await writeFile('.filesystem.json', filesystem);
